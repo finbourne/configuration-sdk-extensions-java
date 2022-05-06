bccdbbae843153434c5b42b373f92cdaa854c7ce
Concourse <concourse@finbourne.com>
2022-05-06 12:18:20 +0000

    Update SDK Extensions 0.1.0 - autogenerated by MustacheGenerator

---
+++ sdk/src/test/unit/java/com/finbourne/configuration/extensions/CredentialsSource.java
@@ -0,0 +1,8 @@
+package com.finbourne.configuration.extensions;
+
+/**
+ * Filename containing configuration API credentials
+ */
+public class CredentialsSource {
+    public static final String credentialsFile = "secrets.json";
+}
